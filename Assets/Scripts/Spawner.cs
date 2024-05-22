d3eb080c59bd687bb5e02ac4179d97b3de223b1b
Adrian Fyrpil <adrian.fyrpihl@hotmail.se>
2024-05-22 16:10:14 +0200

    Fixed some basic logic for the enemy spawns.  Changes to be committed: 	modified:   Assets/Scenes/GameMap3.unity 	modified:   Assets/Scenes/TestSceneZ.unity 	modified:   Assets/Scripts/Spawner.cs

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,19 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.Events;
 
- // Spawner for mobs
+// Spawner for mobs
 public class Spawner : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private GameObject[] enemyprefabs;
 
     [Header("Attributes")]
-    [SerializeField] private int baseEnemies = 8;
+    [SerializeField] private int baseEnemies = 5;
     [SerializeField] private float enemiesPerSecond = 0.5f;
     [SerializeField] private float timeBetweenWaves = 2f;
-    [SerializeField] private float difficultyScalingFactor = 0.75f;
+    [SerializeField] private float difficultyScalingFactor = 0.5f;
 
     [Header("Test")]
     [SerializeField] private int currentWave = 1;
@@ -21,6 +22,8 @@ public class Spawner : MonoBehaviour
     [SerializeField] private int enemiesAlive;
     [SerializeField] private int enemiesLeftToSpawn;
     [SerializeField] private bool isSpawning = false;
+
+    private int curEnemyTypeMax = 1;
 
     [Header("Events")]
     public static UnityEvent onEnemyDestroyed = new UnityEvent();
@@ -53,7 +56,7 @@ public class Spawner : MonoBehaviour
 
         timeSinceLastSpawn += Time.deltaTime;
 
-        if(timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0)
+        if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0)
         {
             SpawnEnemy();
             enemiesLeftToSpawn--;
@@ -61,7 +64,7 @@ public class Spawner : MonoBehaviour
             timeSinceLastSpawn = 0f;
         }
 
-        if(enemiesAlive == 0 && enemiesLeftToSpawn == 0)
+        if (enemiesAlive == 0 && enemiesLeftToSpawn == 0)
         {
             EndWave();
         }
@@ -72,13 +75,25 @@ public class Spawner : MonoBehaviour
         isSpawning = false;
         timeSinceLastSpawn = 0f;
         currentWave++;
+        enemiesPerSecond = 0.5f + 0.1f * currentWave;
+        if (currentWave % 3 == 0 && curEnemyTypeMax <= 3)
+            curEnemyTypeMax += 1;
+
         StartCoroutine(StartWave());
     }
 
     private void SpawnEnemy()
     {
         // Choose a random index within the range of enemyprefabs array
-        int randomIndex = Random.Range(0, enemyprefabs.Length);
+        int randomIndex = Random.Range(0, curEnemyTypeMax);
+        if (currentWave % 5 == 0 && enemiesLeftToSpawn <= currentWave / 5)
+        {
+            randomIndex = 4;
+        }
+        else if (currentWave > 10 && Random.Range(0, 30 - currentWave) == 0)
+        {
+            randomIndex = 4;
+        }
 
         // Get the randomly chosen prefab
         GameObject prefabToSpawn = enemyprefabs[randomIndex];
@@ -86,6 +101,7 @@ public class Spawner : MonoBehaviour
         // Spawn the enemy prefab
         Instantiate(prefabToSpawn, LevelManager.main.StartPoint.position, Quaternion.identity);
     }
+
 
     private int EnemiesPerWave()
     {
